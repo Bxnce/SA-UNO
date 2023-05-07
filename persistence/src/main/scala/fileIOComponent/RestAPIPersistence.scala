6f068b88714a127d9acb046e56bb49c292d0bc17
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2023-05-07 18:39:01 +0200

    add update game and player, delete game and player, removed duplicate code

--- persistence/src/main/scala/fileIOComponent/RestAPIPersistence.scala
+++ persistence/src/main/scala/fileIOComponent/RestAPIPersistence.scala
@@ -1,6 +1,10 @@
 package fileIOComponent
 
+/* Uno-Dependencies*/
 import fileIOComponent.JSONImpl.fileIO
+import fileIOComponent.database.SlickDAO
+import model.gameComponent.gameBaseImpl.{Game, UnoState}
+/*Libaries*/
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
@@ -11,8 +15,6 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
 import akka.stream.ActorMaterializer
 import akka.protobufv3.internal.compiler.PluginProtos.CodeGeneratorResponse.File
-import fileIOComponent.database.Slick
-
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.{Failure, Success}
 import play.api.libs.json.*
@@ -23,7 +25,7 @@ class RestAPIPersistence():
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
   val fileIO = new fileIO
-  val slick = new Slick
+  val slick = new SlickDAO
   val RestUIPort: Int = sys.env.getOrElse("PERSISTENCE_SERVICE_PORT", "8081").toInt
   val RestUIHost: String = sys.env.getOrElse("PERSISTENCE_SERVICE_HOST", "localhost")
 
@@ -43,7 +45,8 @@ class RestAPIPersistence():
       },
       get {
         path("persistence" / "dbload") {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, fileIO.gameToJson(slick.load()).toString()))
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, fileIO.gameToJson(slick.load()
+            .getOrElse(new Game("ERROR LOADING DATABASE", "ERROR LOADING DATABASE", UnoState.winState))).toString()))
         }
       },
       put {
@@ -65,7 +68,93 @@ class RestAPIPersistence():
             }
           }
         }
-      }
+      },
+      put {
+        path("persistence" / "dbupdateplayer") {
+          parameter("id","name".?, "cards".?, "card_count".?, "placed".?) {
+            (id, name, cards, card_count, placed) =>
+            complete {
+              val card_count_updated =
+                card_count match{
+                  case Some(card_count) => Some(card_count.toInt)
+                  case None => None
+                }
+              val placed_updated =
+                placed match{
+                  case Some(placed) => Some(placed.toBoolean)
+                  case None => None
+                }
+              val result = slick.updatePlayer(id=id.toInt, name=name, cards=cards, card_count=card_count_updated,
+                placed = placed_updated).getOrElse("ERROR")
+              Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"updated player database" +
+                s" $result"))
+            }
+          }
+        }
+      },
+      put {
+        path("persistence" / "dbupdategame") {
+          parameter("id", "player1".?, "player2".?, "midcard".?, "currentstate".?, "error".?, "cardstack".?, "winner".?) {
+            (id, player1, player2, midcard, currentstate, error, cardstack, winner) =>
+            complete {
+              val player1_updated =
+                player1 match {
+                  case Some(player1) => Some(player1.toInt)
+                  case None => None
+                }
+              val player2_updated =
+                player2 match {
+                  case Some(player2) => Some(player2.toInt)
+                  case None => None
+                }
+              val midcard_updated =
+                midcard match {
+                  case Some(midcard) => Some(midcard.toInt)
+                  case None => None
+                }
+              val error_updated =
+                error match {
+                  case Some(error) => Some(error.toInt)
+                  case None => None
+                }
+              val winner_updated =
+                winner match {
+                  case Some(winner) => Some(winner.toInt)
+                  case None => None
+                }
+              val result = slick.updateGame(id = id.toInt, player1=player1_updated, player2=player2_updated,
+                midCard=midcard_updated, currentstate=currentstate, error=error_updated, cardstack=cardstack,
+                winner=winner_updated).getOrElse("ERROR")
+              Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"updated game database" +
+                s" $result"))
+            }
+          }
+        }
+      },
+      put {
+        path("persistence" / "dbdeleteplayer") {
+          parameter("id") {
+            (id) =>
+              complete {
+                val result = slick.deletePlayer(id = id.toInt).getOrElse("ERROR")
+                  Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"deleted player in player database" +
+                  s" $result"))
+              }
+          }
+        }
+      },
+      put {
+        path("persistence" / "dbdeletegame") {
+          parameter("id") {
+            (id) =>
+              complete {
+                val result = slick.deleteGame(id = id.toInt).getOrElse("ERROR")
+                Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"deleted game in game database" +
+                  s" $result"))
+              }
+          }
+        }
+      },
     )
 
   def start(): Unit = {
