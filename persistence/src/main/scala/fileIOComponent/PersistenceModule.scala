e396eb7b083b5e27e95b9e9731c0d47366439590
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2023-05-17 14:57:52 +0200

    #17 - mongodb-simple task * removed redundant code * methods that are used more than once are added to MongoHelper.scala * the 3 different handle functions are replaced with asynchronous waiting of the observer parsed to future * added SimpleMongoDAO.scala as a simple implementation of the MongoDAO

--- persistence/src/main/scala/fileIOComponent/PersistenceModule.scala
+++ persistence/src/main/scala/fileIOComponent/PersistenceModule.scala
@@ -1,12 +1,13 @@
 package fileIOComponent
 
 import com.google.inject.AbstractModule
-import fileIOComponent.database.{DAOInterface, MongoDAO, SlickDAO}
+import fileIOComponent.database.{DAOInterface, MongoDAO, SimpleMongoDAO, SlickDAO}
 import net.codingwell.scalaguice.ScalaModule
 
 
 class PersistenceModule extends AbstractModule:
   override def configure(): Unit =
   //bind(classOf[DAOInterface]).to(classOf[SlickDAO])
-    bind(classOf[DAOInterface]).to(classOf[MongoDAO])
+  //bind(classOf[DAOInterface]).to(classOf[MongoDAO])
+    bind(classOf[DAOInterface]).to(classOf[SimpleMongoDAO])
 
---
+++ persistence/src/main/scala/fileIOComponent/database/SimpleMongoDAO.scala
@@ -0,0 +1,92 @@
+package fileIOComponent.database
+
+import com.google.inject.Inject
+import fileIOComponent.JSONImpl.fileIO
+import fileIOComponent.database.{DAOInterface, WAIT_TIME, getHighestId, handleResult}
+import model.gameComponent.gameInterface
+import org.mongodb.scala.bson.BsonDocument
+import org.mongodb.scala.model.*
+import org.mongodb.scala.model.Aggregates.*
+import org.mongodb.scala.model.Filters.*
+import org.mongodb.scala.model.Sorts.*
+import org.mongodb.scala.result.{DeleteResult, InsertOneResult, UpdateResult}
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase, Observable, Observer, SingleObservable, result}
+import play.api.libs.json.{JsObject, Json}
+
+import scala.concurrent.duration.Duration.Inf
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+
+/*
+Simple implementation of a MongoDB DAO - makes more sense, as everything in the database is a JSON anyway
+*/
+class SimpleMongoDAO @Inject() extends DAOInterface {
+  private val fio = new fileIO
+  /* Init */
+  private val database_pw = sys.env.getOrElse("MONGO_ROOT_PASSWORD", "mongo")
+  private val database_username = sys.env.getOrElse("MONGO_ROOT_USERNAME", "root")
+  private val host = sys.env.getOrElse("MONGO_HOST", "localhost")
+  private val port = sys.env.getOrElse("MONGO_PORT", "27017")
+
+  val uri: String = s"mongodb://$database_username:$database_pw@$host:$port/?authSource=admin"
+  private val client: MongoClient = MongoClient(uri)
+  println(uri)
+
+  val db: MongoDatabase = client.getDatabase("uno")
+  private val gameCollection: MongoCollection[Document] = db.getCollection("game")
+  println("Connected to MongoDB")
+
+  override def save(game: gameInterface): Unit =
+    println("Saving game to MongoDB")
+    handleResult(gameCollection.insertOne(Document(
+      "_id" -> (getHighestId(gameCollection) + 1),
+      "game" -> fio.gameToJson(game).toString()
+    )))
+    println(s"Inserted game in MongoDB with id ${getHighestId(gameCollection)}")
+
+  override def load(id: Option[Int]): Try[gameInterface] =
+    Try {
+      fio.jsonToGame(Await.result(gameCollection.find(equal("_id",
+        id.getOrElse(getHighestId(gameCollection)))).first().head(), WAIT_TIME).toString())
+    }
+
+  override def updateGame(id: Int, player1: Option[Int], player2: Option[Int], midCard: Option[Int], currentstate: Option[String], error: Option[Int], cardstack: Option[String], winner: Option[Int]): Try[Boolean] =
+    Try {
+      var gameJson = Json.parse(Await.result(gameCollection.find(equal("_id", id)).first().head(), WAIT_TIME).toString())
+      currentstate match {
+        case Some(value) => gameJson = gameJson.as[JsObject] ++ Json.obj("currentstate" -> value)
+        case None =>
+      }
+      error match {
+        case Some(value) => gameJson = gameJson.as[JsObject] ++ Json.obj("ERROR" -> value)
+        case None =>
+      }
+      cardstack match {
+        case Some(value) => gameJson = gameJson.as[JsObject] ++ Json.obj("cardstack" -> value)
+        case None =>
+      }
+      winner match {
+        case Some(value) => gameJson = gameJson.as[JsObject] ++ Json.obj("winner" -> value)
+        case None =>
+      }
+      handleResult(gameCollection.updateOne(equal("_id", id), Updates.set("game", gameJson)))
+      true
+    }
+
+  override def deleteGame(id: Int): Try[Boolean] =
+    Try {
+      handleResult(gameCollection.deleteOne(equal("_id", id)))
+      true
+    }
+
+  override def updatePlayer(id: Int, name: Option[String], cards: Option[String], card_count: Option[Int], placed: Option[Boolean]): Try[Boolean] =
+    throw new Exception("Not implemented")
+
+  override def deletePlayer(id: Int): Try[Boolean] =
+    throw new Exception("Not implemented")
+
+  private def handleResult[T](obs: SingleObservable[T]): Unit =
+    Await.result(obs.asInstanceOf[SingleObservable[Unit]].head(), WAIT_TIME)
+    println("db operation successful")
+}
